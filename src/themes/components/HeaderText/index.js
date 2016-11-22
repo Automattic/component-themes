3ebca85906839fcbff10bce1c0d4330d51b19560
Payton Swick <payton@foolord.com>
2016-11-22 16:22:43 -0500

    Add requiredApiData to HeaderText component

--- src/themes/components/HeaderText/index.js
+++ src/themes/components/HeaderText/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
-const HeaderText = ( { siteTitle, siteTagline, className } ) => {
+const HeaderText = ( { siteTitle, siteTagline, className, siteInfo } ) => {
 	return (
 		<div className={ className }>
-			<h1 className="HeaderText__title">{ siteTitle || 'My Website' }</h1>
-			<div className="HeaderText__tagline">{ siteTagline || 'My home on the web' }</div>
+			<h1 className="HeaderText__title">{ siteTitle || siteInfo.name || 'My Website' }</h1>
+			<div className="HeaderText__tagline">{ siteTagline || siteInfo.description || 'My home on the web' }</div>
 		</div>
 	);
 };
+
+HeaderText.requiredApiData = { siteInfo: '/' };
 
 HeaderText.description = 'Header containing site title and tagline.';
 HeaderText.editableProps = {
--- src/themes/components/HeaderText/index.php
+++ src/themes/components/HeaderText/index.php
@@ -1,8 +1,9 @@
 <?php
 class ComponentThemes_HeaderText extends ComponentThemes_Component {
-	public function render() {
-		$site_title = $this->getProp( 'siteTitle', 'My Website' );
-		$site_tagline = $this->getProp( 'siteTagline', 'My home on the web' );
+	static $requiredApiData = [ 'siteInfo' => '/' ];
+	public function render( $props ) {
+		$site_title = isset( $props['siteTitle'] ) ? $props['siteTitle'] : isset( $props['siteInfo'] ) ? $props['siteInfo']['name'] : 'My Website';
+		$site_tagline = isset( $props['siteTagline'] ) ? $props['siteTagline'] : isset( $props['siteInfo'] ) ? $props['siteInfo']['description'] : 'My home on the web';
 		return "<div class='" . $this->getProp( 'className' ) . "'>
       <h1 class='HeaderText__title'>$site_title</h1>
       <div class='HeaderText__tagline'>$site_tagline</div>
