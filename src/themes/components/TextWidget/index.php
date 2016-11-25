864c38364abf4785df81cf450f9ab6807054b9f1
Payton Swick <payton@foolord.com>
2016-11-25 17:07:26 -0500

    Fix TextWidget props

--- src/themes/components/TextWidget/index.php
+++ src/themes/components/TextWidget/index.php
@@ -1,9 +1,7 @@
 <?php
-class ComponentThemes_TextWidget extends ComponentThemes_Component {
-	public function render() {
-		$text = isset( $this->props->text ) ? $this->props->text : 'This is a text widget with no data!';
-		$className = isset( $this->props->className ) ? $this->props->className : '';
-		return React::createElement('div',['className' => $className],$text);
-	}
+function ComponentThemes_TextWidget( $props ) {
+	$text = isset( $props->text ) ? $props->text : 'This is a text widget with no data!';
+	$className = isset( $props->className ) ? $props->className : '';
+	return React::createElement('div',['className' => $className],$text);
 }
 
