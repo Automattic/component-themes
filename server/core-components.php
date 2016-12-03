840f5054395aa438a65be8d712105c1e79baaf44
Payton Swick <payton@foolord.com>
2016-12-03 13:40:10 -0500

    Move core component registration to its own PHP file

---
+++ server/core-components.php
@@ -0,0 +1,15 @@
+<?php
+require( __DIR__ . '/../src/themes/components/SearchWidget/index.php' );
+require( __DIR__ . '/../src/themes/components/MenuWidget/index.php' );
+require( __DIR__ . '/../src/themes/components/RowComponent/index.php' );
+require( __DIR__ . '/../src/themes/components/TextWidget/index.php' );
+require( __DIR__ . '/../src/themes/components/PostList/index.php' );
+require( __DIR__ . '/../src/themes/components/PostBody/index.php' );
+require( __DIR__ . '/../src/themes/components/PostTitle/index.php' );
+require( __DIR__ . '/../src/themes/components/PostDate/index.php' );
+require( __DIR__ . '/../src/themes/components/PostAuthor/index.php' );
+require( __DIR__ . '/../src/themes/components/PostContent/index.php' );
+require( __DIR__ . '/../src/themes/components/ColumnComponent/index.php' );
+require( __DIR__ . '/../src/themes/components/FooterText/index.php' );
+require( __DIR__ . '/../src/themes/components/HeaderText/index.php' );
+require( __DIR__ . '/../src/themes/components/PageLayout/index.php' );
