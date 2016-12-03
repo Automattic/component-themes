64a24754286269888f55283bc09bcb3df32bdc9d
Payton Swick <payton@foolord.com>
2016-12-03 13:49:50 -0500

    Use ES6 for app.js since it now goes through babel

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
-/* globals window, require */
-/* eslint-disable no-var */
+/* globals window */
 
 /**
  * External dependencies
  */
-var React = require( 'react' );
-var ReactDOM = require( 'react-dom' );
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 /**
  * Internal dependencies
  */
-var ComponentThemePage = require( '../build' ).ComponentThemePage;
-var apiDataWrapper = require( '../build/lib/api' ).apiDataWrapper;
-var registerComponent = require( '../build/lib/components' ).registerComponent;
-var makeComponentWith = require( '../build/lib/component-builder' ).makeComponentWith;
+import { ComponentThemePage } from '../build';
+import { apiDataWrapper } from '../build/lib/api';
+import { registerComponent } from '../build/lib/components';
+import { makeComponentWith } from '../build/lib/component-builder';
 
-var ComponentThemes = {
+const ComponentThemes = {
 	renderPage: function( theme, slug, page, target ) {
-		var App = React.createElement( ComponentThemePage, { theme, page, slug } );
+		const App = React.createElement( ComponentThemePage, { theme, page, slug } );
 		ReactDOM.render( App, target );
 	},
 
