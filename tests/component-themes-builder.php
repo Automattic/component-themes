5264bc2b022a09d964e7c6459dbda031b69846b7
Payton Swick <payton@foolord.com>
2016-12-05 18:19:54 -0500

    Add tests for rendering a registered component in PHP

--- tests/component-themes-builder.php
+++ tests/component-themes-builder.php
@@ -3,6 +3,15 @@ use function Corretto\describe, Corretto\it, Corretto\expect, Corretto\beforeEac
 
 require( './server/class-component-themes.php' );
 
+// @codingStandardsIgnoreStart
+function Component_Themes_TextWidget( $props ) {
+	// @codingStandardsIgnoreEnd
+	$class = ct_get_value( $props, 'className', '' );
+	$text = ct_get_value( $props, 'text', 'This is a text widget with no data!' );
+	$color = ct_get_value( $props, 'color', 'default' );
+	return React::createElement( 'div', [ 'className' => $class ], [ React::createElement( 'p', [], 'text is: ' . $text ), React::createElement( 'p', [], 'color is: ' . $color ) ] );
+};
+
 describe( 'Component_Themes_Builder', function() {
 	describe( '#render()', function() {
 		describe( 'for an unregistered componentType', function() {
@@ -18,5 +27,32 @@ describe( 'Component_Themes_Builder', function() {
 			} );
 		} );
 
+		describe( 'for a registered componentType', function() {
+			beforeEach( function( $c ) {
+				$c->theme = [ 'name' => 'TestTheme', 'slug' => 'testtheme' ];
+				$c->page = [ 'id' => 'helloWorld', 'componentType' => 'TextWidget', 'props' => [ 'text' => 'hello world' ] ];
+				$c->builder = Component_Themes_Builder::get_builder();
+			} );
+
+			it( 'includes the id as a className', function( $c ) {
+				$result = $c->builder->render( $c->theme, $c->page );
+				expect( $result )->toContain( 'helloWorld' );
+			} );
+
+			it( 'includes the componentType as a className', function( $c ) {
+				$result = $c->builder->render( $c->theme, $c->page );
+				expect( $result )->toContain( 'TextWidget' );
+			} );
+
+			it( 'includes the props passed in the object description', function( $c ) {
+				$result = $c->builder->render( $c->theme, $c->page );
+				expect( $result )->toContain( 'text is: hello world' );
+			} );
+
+			it( 'includes props not passed in the object description as falsy values', function( $c ) {
+				$result = $c->builder->render( $c->theme, $c->page );
+				expect( $result )->toContain( 'color is: default' );
+			} );
+		} );
 	} );
 } );
