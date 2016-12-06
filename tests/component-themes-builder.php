b97d1ee399b493abe98dd0be430148ce7d0fe6ba
Payton Swick <payton@foolord.com>
2016-12-05 20:11:43 -0500

    Add PHP tests for merge_themes

--- tests/component-themes-builder.php
+++ tests/component-themes-builder.php
@@ -19,6 +19,9 @@ class Component_Themes_ColumnComponent extends Component_Themes_Component {
 	}
 }
 
+$test_partial = [ 'id' => 'helloWorld', 'componentType' => 'TextWidget', 'props' => [ 'text' => 'test partial' ] ];
+Component_Themes_Builder::register_partial( 'TestPartial', $test_partial );
+
 describe( 'Component_Themes_Builder', function() {
 	beforeEach( function( $c ) {
 		$c->builder = Component_Themes_Builder::get_builder();
@@ -102,8 +105,6 @@ describe( 'Component_Themes_Builder', function() {
 			beforeEach( function( $c ) {
 				$c->theme = [ 'name' => 'TestTheme', 'slug' => 'testtheme' ];
 				$c->page = [ 'id' => 'layout', 'componentType' => 'ColumnComponent', 'children' => [ [ 'id' => 'existing', 'componentType' => 'TextWidget' ], [ 'partial' => 'TestPartial' ] ] ];
-				$test_partial = [ 'id' => 'helloWorld', 'componentType' => 'TextWidget', 'props' => [ 'text' => 'test partial' ] ];
-				$c->builder->register_partial( 'TestPartial', $test_partial );
 			} );
 
 			it( 'does not affect sibling components', function( $c ) {
@@ -115,6 +116,55 @@ describe( 'Component_Themes_Builder', function() {
 				$result = $c->builder->render( $c->theme, $c->page );
 				expect( $result )->toContain( 'helloWorld' );
 			} );
+		} );
+	} );
+
+	describe( '#merge_themes()', function() {
+		beforeEach( function( $c ) {
+			$c->theme1 = [ 'name' => 'First Theme', 'slug' => 'first', 'templates' => [ 'firstTemplate' => [ 'id' => 'helloWorld', 'componentType' => 'TextWidget', 'props' => [ 'text' => 'first text' ] ], 'mergingTemplate' => [ 'id' => 'toBeOverwritten', 'componentType' => 'TextWidget' ] ] ];
+			$c->theme2 = [ 'name' => 'Second Theme', 'partials' => [], 'templates' => [ 'secondTemplate' => [ 'id' => 'helloWorld', 'componentType' => 'TextWidget', 'props' => [ 'text' => 'second text' ] ], 'mergingTemplate' => [ 'id' => 'overwriter', 'componentType' => 'TextWidget' ] ] ];
+		} );
+
+		it( 'includes the keys of both themes', function( $c ) {
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( array_keys( $theme ) )->toContain( 'name' );
+			expect( array_keys( $theme ) )->toContain( 'slug' );
+			expect( array_keys( $theme ) )->toContain( 'partials' );
+			expect( array_keys( $theme ) )->toContain( 'templates' );
+		} );
+
+		it( 'includes the templates in the first theme', function( $c ) {
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( array_keys( $theme['templates'] ) )->toContain( 'firstTemplate' );
+		} );
+
+		it( 'includes the templates in the second theme', function( $c ) {
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( array_keys( $theme['templates'] ) )->toContain( 'secondTemplate' );
+		} );
+
+		it( 'overwrites string properties of the first theme with properties of the second', function( $c ) {
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( $theme['name'] )->toEqual( 'Second Theme' );
+		} );
+
+		it( 'overwrites number properties of the first theme with properties of the second', function( $c ) {
+			$c->theme1['name'] = 4;
+			$c->theme2['name'] = 8;
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( $theme['name'] )->toEqual( 8 );
+		} );
+
+		it( 'overwrites array properties of the first theme with properties of the second', function( $c ) {
+			$c->theme1['name'] = [ 'a' ];
+			$c->theme2['name'] = [ 'b' ];
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( $theme['name'] )->toEqual( [ 'b' ] );
+		} );
+
+		it( 'overwrites object property properties of the first theme with those of the second', function( $c ) {
+			$theme = $c->builder->merge_themes( $c->theme1, $c->theme2 );
+			expect( $theme['templates']['mergingTemplate']['id'] )->toEqual( 'overwriter' );
 		} );
 	} );
 } );
