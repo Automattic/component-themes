3cd2054ad2e53bc496ab7b80378c9a5a22430268
Payton Swick <payton@foolord.com>
2016-12-11 17:06:52 -0500

    Use React::renderChildren for render_children

--- server/class-component-themes-component.php
+++ server/class-component-themes-component.php
@@ -1,40 +1,19 @@
 <?php
 class Component_Themes_Component {
-	protected $builder;
+	public $props;
+	public $children;
 
 	public function __construct( $props = [], $children = [] ) {
 		$this->props = $props;
 		$this->children = $children;
-		$this->builder = Component_Themes_Builder::get_builder();
 	}
 
 	public function get_prop( $key, $default = null ) {
 		return ct_get_value( $this->props, $key, $default );
 	}
 
-	public function get_prop_from_parent( $key, $default = null ) {
-		$child_props = ct_get_value( $this->props, 'child_props', [] );
-		return ct_get_value( $child_props, $key, $default );
-	}
-
-	public function make_component_with( $config, $data ) {
-		return $this->builder->make_component_with( $config, $data );
-	}
-
-	protected function render_child( $child ) {
-		if ( is_string( $child ) ) {
-			return $child;
-		}
-		return $child->render();
-	}
-
 	public function render_children() {
-		if ( ! is_array( $this->children ) ) {
-			return $this->render_child( $this->children );
-		}
-		return implode( ' ', array_map( function( $child ) {
-			return $this->render_child( $child );
-		}, $this->children ) );
+		return React::renderChildren( $this->children );
 	}
 
 	public function render() {
