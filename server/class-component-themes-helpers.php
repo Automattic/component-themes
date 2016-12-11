f395707c30320ab80ff8171860a723c62d1010f6
Payton Swick <payton@foolord.com>
2016-12-11 18:59:13 -0500

    Add ct_array_flatten

--- server/class-component-themes-helpers.php
+++ server/class-component-themes-helpers.php
@@ -8,3 +8,15 @@ function ct_get_value( $ary, $key, $default = null ) {
 function ct_or( $val, $default ) {
 	return ! empty( $val ) ? $val : $default;
 }
+
+function ct_array_flatten( $ary ) {
+	$result = [];
+	foreach ( $ary as $val ) {
+		if ( is_array( $val ) ) {
+			$result = array_merge( $result, $val );
+		} else {
+			$result[] = $val;
+		}
+		return $result;
+	}
+}
