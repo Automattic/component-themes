fee1341a7c5103c1b0ff2a8eddbd59164ab1cb31
Payton Swick <payton@foolord.com>
2016-12-13 12:15:49 -0500

    Add label and button elements to PHP SearchWidget

--- src/themes/components/SearchWidget/index.php
+++ src/themes/components/SearchWidget/index.php
@@ -4,9 +4,17 @@ $search_widget = function ( $props ) {
 	$class_name = ct_get_value( $props, 'className', '' );
 	// TODO: import the rootUrl from the state
 	$root_url = '/';
+	$label = ct_get_value( $props, 'label' );
+	$label_element = empty( $label ) ? null : React::createElement( 'span', [ 'className' => 'screen-reader-text' ], $label );
+	$button_label = ct_get_value( $props, 'buttonLabel' );
+	$button_element = empty( $button_label ) ? null : React::createElement( 'submit', [ 'className' => 'SearchWidget__button' ], $button_label );
 	return React::createElement( 'div', [ 'className' => $class_name ], [
 		React::createElement( 'form', [ 'className' => 'SearchWidget__form', 'action' => $root_url, 'method' => 'get', 'role' => 'search' ], [
-			React::createElement( 'input', [ 'className' => 'SearchWidget__input', 'placeholder' => $placeholder, 'name' => 's' ] ),
+			React::createElement( 'label', [], [
+				$label_element,
+				React::createElement( 'input', [ 'className' => 'SearchWidget__input', 'placeholder' => $placeholder, 'name' => 's' ] ),
+			] ),
+			$button_element,
 		] ),
 	] );
 };
