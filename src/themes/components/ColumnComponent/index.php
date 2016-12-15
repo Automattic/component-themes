d62ebcfc77f35bd47c91a04377548ef519298c45
Taegon Kim <gonom9@gmail.com>
2016-12-16 02:17:36 +0900

    Remove array shorthand syntax from ColumnComponent class.

--- src/themes/components/ColumnComponent/index.php
+++ src/themes/components/ColumnComponent/index.php
@@ -1,9 +1,9 @@
 <?php
 function Component_Themes_Column_Component( $props, $children ) {
 	$class_name = ct_get_value( $props, 'className', '' );
-	$new_props = ct_omit( $props, [ 'className', 'children' ] );
+	$new_props = ct_omit( $props, array( 'className', 'children' ) );
 	$new_children = React::cloneChildren( $chilren, $new_props );
-	return React::createElement( 'div', [ 'className' => $class_name ], $new_children );
+	return React::createElement( 'div', array( 'className' => $class_name ), $new_children );
 };
 
 Component_Themes::register_component( 'ColumnComponent', 'Component_Themes_Column_Component' );
