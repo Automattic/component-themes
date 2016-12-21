a54c344b3ea993caab933e1e3e001e1c88347478
Taegon Kim <gonom9@gmail.com>
2016-12-21 23:00:30 +0900

    Now PostDate component formats date in client-side.

--- src/themes/components/PostDate/index.js
+++ src/themes/components/PostDate/index.js
@@ -1,16 +1,27 @@
 /* globals window */
 const ComponentThemes = window.ComponentThemes;
-const { React, registerComponent } = ComponentThemes;
+const { React, registerComponent, apiDataWrapper } = ComponentThemes;
 
-const PostDate = ( { date, className } ) => {
+const PostDate = ( { date, date_format, className } ) => {
+	var dateStr = '';
+
+	if ( date ) {
+		dateStr = ComponentThemes.date.format( date_format, new Date( date ) );
+	}
+
 	return (
 		<span className={ className }>
-			{ date || 'No date' }
+			{ dateStr || 'No date' }
 		</span>
 	);
 };
 
-registerComponent( 'PostDate', PostDate, {
+const mapApiToProps = ( getApiEndpoint ) => {
+	const settings = getApiEndpoint( '/wp/v2/settings' ) || [];
+	return { date_format: settings.date_format };
+};
+
+registerComponent( 'PostDate', apiDataWrapper( mapApiToProps )( PostDate ), {
 	title: 'Post Date',
 	description: 'The date for a post. Use inside a PostBody.',
 	editableProps: {
