39c31becc85a8a479f1836f2d5af5ed83a1418ab
Taegon Kim <gonom9@gmail.com>
2017-01-06 20:31:46 +0900

    Add test cases for TextWidget component

--- src/themes/components/TextWidget/index.php
+++ src/themes/components/TextWidget/index.php
@@ -2,7 +2,9 @@
 function Component_Themes_TextWidget( $props ) {
 	$text = ct_get_value( $props, 'text', 'This is a text widget with no data!' );
 	$class_name = ct_get_value( $props, 'className', '' );
-	return React::createElement( 'div', array( 'className' => $class_name ), $text );
+	$new_props = $class_name ? array( 'className' => $class_name ) : array();
+
+	return React::createElement( 'div', $new_props, $text );
 }
 
 Component_Themes::register_component( 'TextWidget', 'Component_Themes_TextWidget' );
